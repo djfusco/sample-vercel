f859bf980ce2b8fad9825818e68ff1e670e1e128
djfusco <davidjfusco@gmail.com>
2023-12-20 11:30:17 -0500

    back to plain
    plain

--- api/newroute.js
+++ api/newroute.js
@@ -1,12 +1,3 @@
-//import { OpenAI } from 'openai';
-import { Configuration, OpenAIApi } from "openai";
-
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
-
-
 export default async function handler(req, res) {
     const search = req.query.search || '';
     const rosterInfo = "inside api new";
