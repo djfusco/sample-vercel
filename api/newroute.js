caf84e7d497476210b129ca27df1ba3b713f919c
djfusco <davidjfusco@gmail.com>
2023-12-20 10:38:38 -0500

    first step
    first

--- api/newroute.js
+++ api/newroute.js
@@ -1,3 +1,5 @@
+import OpenAI from "openai";
+const openai = new OpenAI({apiKey: process.env.CHAGPT_KEY});
 
 export default async function handler(req, res) {
     const search = req.query.search || '';
