fd209e8fc8018271a4ae5f92c208b33611daf345
djfusco <davidjfusco@gmail.com>
2023-12-20 10:33:44 -0500

    added api
    api

---
+++ api/newroute.js
@@ -0,0 +1,12 @@
+
+export default async function handler(req, res) {
+    const search = req.query.search || '';
+    const rosterInfo = "inside api";
+    console.log(rosterInfo);
+    res.setHeader('Cache-Control', 'max-age=0, s-maxage=1800');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+    res.json(rosterInfo);
+  }
